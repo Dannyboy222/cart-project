c47644ea69e654982f642c15b9b97b8497bf4b33
Dannyboy222 <76968917+Dannyboy222@users.noreply.github.com>
2021-03-24 13:03:10 -0700

    changed some codes (html css)

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ function() {
         cart.classList.toggle('show-cart');
     })
 }
+
+//adding items to the cart
+function(){
+    const cartBtn = document.querySelectorAll('.store-item-icon');
+
+    cartBtn.forEach(function(btn){
+        btn.addEventListener('click', function(event){
+            console.log(event.target);
+        })
+    })
+}
