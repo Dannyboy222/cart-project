1e803acc4dc562226e34361e0ce775616bbd6f1b
Dannyboy222 <76968917+Dannyboy222@users.noreply.github.com>
2021-03-25 09:44:43 -0700

    added products file

---
+++ products.js
@@ -0,0 +1,81 @@
+// Good Pizza menu (PRODUCT ID : DATA)
+var products = {
+    123: {
+      name : "6 Cheese pizza",
+      desc : "Greatest properly off ham exercise all.",
+      img : "6cheese pizza.jpg",
+      price : 18.99
+    },
+    124: {
+      name : "BBQ chicken",
+      desc : "Unsatiable its possession nor off.",
+      img : "BBQ chicken.jpg",
+      price : 18.99
+    },
+    125: {
+      name : "Brooklyn pizza",
+      desc : "All difficulty unreserved the solicitude.",
+      img : "brooklyn pizza.jpg",
+      price : 18.99
+    },
+    126: {
+      name : "Canadian pizza",
+      desc : "Had judgment out property the supplied. ",
+      img : "canadian pizza.jpg",
+      price : 18.99
+    },
+    127: {
+        name : "Cheeseburger",
+        desc : "Had judgment out property the supplied. ",
+        img : "cheeseburger.jpg",
+        price : 18.99
+    },
+    128: {
+        name : "Chicken bacon",
+        desc : "Had judgment out property the supplied. ",
+        img : "chicken bacon.jpg",
+        price : 18.99
+    },
+    129: {
+        name : "Chicken taco",
+        desc : "Had judgment out property the supplied. ",
+        img : "chicken taco.jpg",
+        price : 18.99
+    },
+    130: {
+        name : "Deluxe",
+        desc : "Had judgment out property the supplied. ",
+        img : "deluxe",
+        price : 18.99
+    },
+    131: {
+        name : "Hawaiian",
+        desc : "Had judgment out property the supplied. ",
+        img : "hawaiian.jpg",
+        price : 18.99
+    },
+    132: {
+        name : "Meatzza",
+        desc : "Had judgment out property the supplied. ",
+        img : "meatzza.jpg",
+        price : 18.99
+    },
+    133: {
+        name : "Mediterranean",
+        desc : "Had judgment out property the supplied. ",
+        img : "mediterranean.jpg",
+        price : 18.99
+    },
+    134: {
+        name : "Pepperoni",
+        desc : "Had judgment out property the supplied. ",
+        img : "pepperoni",
+        price : 18.99
+    },
+    135: {
+        name : "Philly steak",
+        desc : "Had judgment out property the supplied. ",
+        img : "philly steak.jpg",
+        price : 18.99
+    },
+  };
